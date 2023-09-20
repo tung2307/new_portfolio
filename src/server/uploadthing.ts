700629c5ab8553c11fa435505b40c72c8ebfd86b
Tung <tung@Tungs-MacBook-Pro.local>
2023-09-19 22:54:57 -0700

    resolve Eslint

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -1,19 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
+import { createUploadthing, FileRouter } from "uploadthing/next-legacy";
 
 const f = createUploadthing();
 
 const auth = (req: NextApiRequest, res: NextApiResponse) => ({ id: "fakeId" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
-export const ourFileRouter = {
+export const ourFileRouter: FileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   videoUploader: f({ video: { maxFileSize: "128MB" } })
     // Set permissions and file types for this FileRoute
-    .middleware(async ({ req, res }) => {
+    .middleware(({ req, res }) => {
       // This code runs on your server before upload
-      const user = await auth(req, res);
+      const user = auth(req, res);
 
       // If you throw, the user will not be able to upload
       if (!user) throw new Error("Unauthorized");
@@ -21,12 +21,12 @@ export const ourFileRouter = {
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.id };
     })
-    .onUploadComplete(async ({ metadata, file }) => {
+    .onUploadComplete(({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
 
       console.log("file url", file.url);
     }),
-} satisfies FileRouter;
+};
 
 export type OurFileRouter = typeof ourFileRouter;
